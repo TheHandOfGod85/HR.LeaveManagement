c26076953ba273cb49a7839b1a5333b4afe85008
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-11-19 11:03:30 +0000

    Add project files.

---
+++ HR.LeaveManagement.Application/DTOs/Common/LeaveAllocationDto.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.Common;
+
+public class LeaveAllocationDto : BaseDto
+{
+    public int NumberOfDays { get; set; }
+    public LeaveTypeDto LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+}
---
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain.Common;
+
+public abstract class BaseDomainEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string? ModifiedBy { get; set; }
+}
